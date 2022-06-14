5c99659da947bbf76b9fd9b4d339352b2f1e0224
spark <isspark@163.com>
2022-06-14 19:38:36 +0800

    first commit

---
+++ src/test/java/com/isspark/cloud/CloudStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isspark.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
